33a7e75dce9c3340631830dce132a3f81816fe50
vivek <vivekranjan8789@gmail.com>
2024-04-10 00:19:15 +0530

    Added create and veify token of jwt

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const {UserRepository} = require('./repository/user-repository');
-// const {user} = require('./models/index');
-// const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -18,13 +15,7 @@ const prepareAndStartServer = () => {
 
        app.listen(3001,async ()=>{
          console.log(`Server Started at: ${PORT}`);
-        //  const incomingpassword = '123456'
-        //  const repo = new UserRepository();
-        //   const users = await repo.getById(4);
-        //  const response = bcrypt.compareSync(incomingpassword, users.password);
-        //  console.log(response);
-        //  console.log(users);
-
+         
        })
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
+var jwt = require('jsonwebtoken');
+
 const { UserRepository } = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService  {
     constructor(){
@@ -12,6 +15,26 @@ class UserService  {
         } catch (error) {
             console.log("something went wrong on service layer");
             throw error;           
+        }
+    }
+
+    createToken(user){
+        try {
+            var token= jwt.sign(user,JWT_KEY, {expiresIn: 30});
+            return token;           
+        } catch (error) {
+            console.log("something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("something went wrong in token validation");
+            throw(error);
         }
     }
 }
